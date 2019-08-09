bf8a9caf31620fc47141483c69233771f1c8344e
angrypie <cookies.in.chocolate@gmail.com>
2019-08-09 10:34:24 +0300

    Display one file if torrent is not a directory

--- src/routes/Playlist/api.js
+++ src/routes/Playlist/api.js
@@ -37,6 +37,11 @@ export async function getAudio(ih, pathArr) {
 }
 
 async function transformTorrentInfo(ih, torrent) {
+	console.log(torrent)
+	if(!torrent.files) {
+		const { length, name } = torrent
+		torrent.files = [ { name, length, path: [name] } ]
+	}
 	for (const file of torrent.files) {
 		const cached = await storage.getFile(ih, file.path.join('/'))
 		file.cached = cached !== null
--- src/routes/Playlist/index.js
+++ src/routes/Playlist/index.js
@@ -5,7 +5,10 @@ import { Nav } from './Nav'
 import { useLocalStore } from 'mobx-react-lite'
 
 function App() {
-	const ih = '5A0EEED914F871D1FDDA8A0E11B1186E3F4AB6DB'
+	//Antony
+	//const ih = '5A0EEED914F871D1FDDA8A0E11B1186E3F4AB6DB'
+	//1984
+	const ih = '2336697F0C677ACEA47608E884CD584F9480B26B'
 
 	const store  = useLocalStore(() => ({
 		ih,
